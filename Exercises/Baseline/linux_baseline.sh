e0dd899dd6332d2d7e67b37b082eca5035bcc892
devurandom11 <dev.random.87@gmail.com>
2025-03-10 22:36:15 -0700

    Initial Commit

---
+++ Exercises/Baseline/linux_baseline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This program performs a system baseline for cybersecurity competitions
+
+# Functions
+banner() {
+	echo "Create Banner"
+}
+
+print_help() {
+	echo "Print help menu"
+}
+
+# Main program
+#
+main() {
+	echo "Main Program..."
+}
